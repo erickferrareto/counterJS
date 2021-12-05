2d02e79093f034d24ebc6dd5ee2e74c1c2af68b9
Erick Ferrareto <erickskate38@gmail.com>
2021-12-04 22:28:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let resposta = document.getElementById('resposta').innerHTML = 0
+function adicionar(){
+  let number = document.getElementById('resposta').innerHTML = resposta++
+}
+function resetar(){
+  location.reload()
+}
+
+function diminuir(){
+  let number = document.getElementById('resposta').innerHTML = resposta--
+}
