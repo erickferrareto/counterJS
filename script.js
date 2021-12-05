a8e13129bfa00c1f7fff45b7cbde27f5cf8dfcfc
Erick Ferrareto <erickskate38@gmail.com>
2021-12-05 15:23:42 -0300

    final process

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let resposta = document.getElementById('resposta').innerHTML = 0
 function adicionar(){
-  let number = document.getElementById('resposta').innerHTML = resposta++
+  let number = document.getElementById('resposta').innerHTML = resposta += 1
 }
 function resetar(){
   location.reload()
 }
 
 function diminuir(){
-  let number = document.getElementById('resposta').innerHTML = resposta--
+  let number = document.getElementById('resposta').innerHTML = resposta -= 1
 }
